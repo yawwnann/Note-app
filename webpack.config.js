97d7d3c3e40945a4660b05517d2b67b6d1ccd544
yawwnann <c0478592@gmail.com>
2025-04-26 01:14:13 +0700

    package

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,34 @@
-// webpack.config.js
+// webpack.config.js (Tanpa Komentar)
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // Mode: 'development' atau 'production'
-  // Ditentukan via script npm, tapi bisa set default di sini
-  // mode: 'development', // Sebaiknya diatur via script npm
-
-  // Entry point aplikasi Anda
-  entry: "./src/index.js", // Pastikan file ini ada di src/
-
-  // Konfigurasi output bundle
+  entry: "./src/index.js",
   output: {
-    filename: "bundle.js", // Nama file bundle output
-    path: path.resolve(__dirname, "dist"), // Folder output (akan dibuat jika belum ada)
-    clean: true, // Bersihkan folder dist sebelum build (webpack 5+)
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-
-  // Konfigurasi module loaders
   module: {
     rules: [
       {
-        test: /\.css$/i, // Terapkan loader untuk file .css
-        // Loader dieksekusi dari kanan ke kiri: css-loader -> style-loader
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      // Anda bisa menambahkan loader lain di sini (misal babel-loader jika diperlukan)
     ],
   },
-
-  // Konfigurasi plugin
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // Gunakan file src/index.html sebagai template
-      filename: "index.html", // Nama file HTML output di folder dist
+      template: "./src/index.html",
+      filename: "index.html",
     }),
   ],
-
-  // Konfigurasi webpack-dev-server
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"), // Folder yang di-serve oleh devServer
+      directory: path.join(__dirname, "dist"),
     },
-    compress: true, // Aktifkan kompresi gzip
-    port: 9000, // Port server (bisa ganti sesuai keinginan)
-    open: true, // Buka browser otomatis saat server jalan
-    // hot: true, // Bisa diaktifkan untuk Hot Module Replacement
-    // client: {
-    //   overlay: { // Menampilkan error langsung di browser
-    //     errors: true,
-    //     warnings: false,
-    //   },
-    // },
+    compress: true,
+    port: 9000,
+    open: true,
   },
 };
